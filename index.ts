9131fc94792bfb153a8542a74e682769289238e2
Zachary Siefkes <zacharysiefkes@gmail.com>
2021-03-26 14:57:33 +1300

    Serve object as static website

--- index.ts
+++ index.ts
@@ -3,15 +3,21 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as fs from "fs";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Create an AWS resource (S3 Bucket) and set website property
+const bucket = new aws.s3.Bucket("my-bucket", {
+    website: {
+        indexDocument: "index.html"
+    }
+});
 
 // Create a bucket object
 const bucketObject = new aws.s3.BucketObject("index.html", {
+    acl: "public-read",
     bucket: bucket,
-    content: fs.readFileSync("site/index.html").toString()
+    content: fs.readFileSync("site/index.html").toString(),
+    contentType: "text/html"
 });
 
-// Export the name of the bucket
+// Export bucket name and endpoint
 export const bucketName = bucket.id;
-
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
