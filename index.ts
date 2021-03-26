db0aa2bd894459180f60738786792d2300f7bbfd
Zachary Siefkes <zacharysiefkes@gmail.com>
2021-03-26 14:30:10 +1300

    Add bucket object

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as fs from "fs";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
+
+// Create a bucket object
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket,
+    content: fs.readFileSync("site/index.html").toString()
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
